852a33417cf2a85cfd68d21ef7edfbd6fac91015
Faizila Amlani <amlani.faizila@gmail.com>
2021-09-22 17:03:56 -0500

    updated script.js initial code

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,72 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// Assigned Variables
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var special = "!@#$%^&*,.+=(){}[]/";
+var Characters = "";
 
-  passwordText.value = password;
+
+
+// Event Listener added to generate button
+generateBtn.addEventListener("click", writePassword);
+
+//generate password function gets called in and it would return the final password.
+function generatePassword(){
+  var result = "";
+
+  //User answers a question that how many characters they want in their password between 8-128 characters.
+  var length = prompt("How many characters do you want in your password? Please choose numbers between 8 and 128!");
+  if(isNaN(length)){
+    alert("You must enter a number!");
+    return generatePassword()
+  }
+  if(length<8 || length> 128){
+    alert("Please choose numbers between 8 - 128!");
+    return generatePassword()
+  }
+//User answers few questions like if they want to include or not want to include upper case, lower case, number or special characters?
+  var incUpper = confirm("Do you want to include upper case letters?");
+  var incLower = confirm("Do you want to include lower case letters?");
+  var incNumbers = confirm("Do you want to include numbers?");
+  var incSpecial = confirm("Do you want to include special characters?");
+
+//User will get an alert to choose at least one character type.
+  if(!incUpper&&!incLower&&!incNumbers&&!incSpecial){
+    alert("You must choose at least one character type!");
+    return generatePassword()
+  }
+// Conditional statements
+  if(incUpper){
+    Characters += upper
+  }
+  if(incLower){
+    Characters += lower
+  }
+
+  if(incNumbers) {
+    Characters += numbers
+  }
+
+  if(incSpecial) {
+    Characters += special
+  }
+  
+// Result
+for (var i = 0; i < length; i++) {
+  result += Characters.charAt(Math.floor(Math.random() * Characters.length));
+}
+return result;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  
+  }
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Password Generator
-Automatically generates a password based on guidelines that are set to create unpredictable passwords.
+# JavaScript: Password Generator
+
+* This application automatically generates a password based on guidelines that are set to create random passwords.
+
+# Description
+
+* On Password Generator webpage, when user clicks "generate password" button, then the user is prompted to enter numbers between 8 and 128 characters in length. It then prompts the user to enter their preferences. For instance, whether a user want to include or don't want to include upper and lower case letters, numbers and special characters to generate their random password. When the questionaire is complete, the application generates a random password for the user.
+
+## Technologies Used
+
+* HTML, CSS & JavaScript
+
+## Link of Deployed Application
+
+
+## Screenshots of Deployed Application
+
+![screenshot1](https://user-images.githubusercontent.com/78191579/134427994-df663180-ecbe-45f0-a44e-17d6d23e1788.JPG)
+![screenshot2](https://user-images.githubusercontent.com/78191579/134427999-ad3355c1-59a5-4b1e-9958-3ed8d0422966.JPG)
+![screenshot3](https://user-images.githubusercontent.com/78191579/134428004-f0ce4347-963b-42ac-986d-65a1cd8de101.JPG)
+![screenshot4](https://user-images.githubusercontent.com/78191579/134428009-74d760cb-e894-4191-a886-b42936513c23.JPG)
+![screenshot5](https://user-images.githubusercontent.com/78191579/134428016-d6c348fe-6cc2-4f28-a9ec-fa968b787a05.JPG)
+![screenshot6](https://user-images.githubusercontent.com/78191579/134428022-ef6ecbb4-5f61-4580-bd51-20ae017a9d71.JPG)
+![screenshot7](https://user-images.githubusercontent.com/78191579/134428025-9ed1f0db-8f0a-4199-8b34-58c983beb706.JPG)
+
+
+
