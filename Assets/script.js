5fbe14a54f44dda0bca4456db080d79280cced64
Faizila Amlani <amlani.faizila@gmail.com>
2021-09-23 15:20:19 -0500

    updated script.js

--- Assets/script.js
+++ Assets/script.js
@@ -23,36 +23,36 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
   var finalResult = "";
 
-  //Prompt for user
+//Prompt for user
   var length = prompt(
     "How many characters do you want in your password? Please choose numbers between 8 and 128!"
   );
 
-  // function determines whether a value is a legal number or not.
+// function determines whether a value is a legal number or not.
   if (isNaN(length)) {
     alert("You must enter a number!");
     return generatePassword();
   }
 
-  // user will get an alert if they pick numbers outside the mentioned range.
+// user will get an alert if they pick numbers outside the mentioned range.
   if (length < 8 || length > 128) {
     alert("Please choose numbers between 8 - 128!");
     return generatePassword();
   }
 
-  // Prompt for user to answer few questions
+// Prompt for user to answer few questions
   var incUpper = confirm("Do you want to include upper case letters?");
   var incLower = confirm("Do you want to include lower case letters?");
   var incNumbers = confirm("Do you want to include numbers?");
   var incSpecial = confirm("Do you want to include special characters?");
 
-  //User will get an alert to choose at least one character type
+//User will get an alert to choose at least one character type
   if (!incUpper && !incLower && !incNumbers && !incSpecial) {
     alert("You must choose at least one character type!");
     return generatePassword();
   }
 
-  // Conditional statements
+// Conditional statements
   if (incUpper) {
     Characters += upper;
   }
@@ -68,9 +68,10 @@ function generatePassword() {
     Characters += special;
   }
 
-  // Result
+// Result
   for (var i = 0; i < length; i++) {
-    finalResult += Characters.charAt(Math.floor(Math.random() * Characters.length)
+    finalResult += Characters.charAt(
+      Math.floor(Math.random() * Characters.length)
     );
   }
   return finalResult;
